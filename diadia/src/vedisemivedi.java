ad87d4f0b6146c4a3cf3a1540acbf5fc44931bc9
Pietro Savelli Portatile <pietrosavelli04@gmail.com>
2025-03-21 16:08:48 +0100

    prima modifica da portatile

--- diadia/src/vedisemivedi.java
+++ diadia/src/vedisemivedi.java
@@ -5,6 +5,11 @@ public class vedisemivedi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("pietro frociona da combattimento");
+			
+			
+			
+			
+			System.out.println("pietro portatile(vedi se mi vedi)");
 	}
 
 }
