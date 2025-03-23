695d7bf203f2b870b7053abb0bd26193ed415cf5
Pietro_Savelli <pietrosavelli04@gmail.com>
2025-03-23 19:40:48 +0100

    fdsfsdfsd

--- diadia/src/diadia/vedisemivedi.java
+++ diadia/src/diadia/vedisemivedi.java
@@ -1,11 +1,3 @@
 package diadia;
 
 
-public class vedisemivedi {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("pietro frociona da combattimento");
-	}
-
-}
